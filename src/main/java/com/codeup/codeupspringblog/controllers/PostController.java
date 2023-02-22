c7b933603d7d000d0d155878a7ecd5c609f56a7b
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2023-02-22 15:56:01 -0700

    adding views exercise

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,23 +1,33 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts(){
-        return "Posts index page.";
+    public String posts(Model model){
+        ArrayList<Post> posts = new ArrayList<>();
+        Post post1 = new Post("Post Number 1", "Free stuff all over the place");
+        Post post2 = new Post("Post Number 2", "Free stuff in some places.");
+        posts.add(post1);
+        posts.add(post2);
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postsId(@PathVariable int id){
-        return String.format("Viewing individual post with an id of %d.", id);
+    public String postsId(@PathVariable int id, Model model) {
+        Post post = new Post("Free Stuff1", "2Alot of free stuff.");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
---
+++ src/main/java/com/codeup/codeupspringblog/controllers/RollDiceController.java
@@ -0,0 +1,21 @@
+package com.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class RollDiceController {
+
+    @GetMapping("/roll-dice")
+    public String guessForm(){
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String roll(@PathVariable int n, Model model) {
+        model.addAttribute("n", n);
+        model.addAttribute("result", (int)(Math.floor(Math.random()*6)+1));
+        return "roll-result";
+    }
+}
